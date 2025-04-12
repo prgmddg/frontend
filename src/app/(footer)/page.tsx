bf7b79fc804c3e5b47e0878f91acd3adf241546a
david <david.cesar2126@gmail.com>
2025-04-12 11:15:52 -0500

    fixed modal

--- src/app/(footer)/page.tsx
+++ src/app/(footer)/page.tsx
@@ -1,11 +1,33 @@
+'use client'
+
 import {MostrarCards, Stripe} from '@/old-components/Servicios'
 import {Carosuel, BeneficiosDeNuestrosProgramas, CentroDeCapacitacion, CertificiacionIso} from '..'
 import BannerStripe from '../old-components/BannerStripe/BannerStripe'
 import WhatsAppIcon from '@/app/old-components/WhatsAppIcon'
+import { useEffect, useState } from 'react'
+import getRequest from '@/helpers/getRequest'
+import InitialPopups from '../old-components/InitialPopups/InitialPopups'
 
 export default function Home() {
+  const [popups, setPopups] = useState<any>()
+  const [errPopup, setErrPopup] = useState<any>()
+ 
+  useEffect(() => {
+    getRequest('modal')
+      .then((response) => {
+        setPopups(response.res)
+        setErrPopup(response.err)
+ 
+        if (window.scrollY > 925) window.scrollTo(0, 925)
+      })
+      .catch((error) => console.error(error))
+  }, [])
+
   return (
     <>
+      {popups?.length > 0 && !errPopup && (
+        <InitialPopups initialPopups={popups} />
+      )}
       <WhatsAppIcon/>
       <BannerStripe/>
       <Carosuel/>
