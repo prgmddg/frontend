d6debf8054062a3984c200307caadb357d65c291
david <david.cesar2126@gmail.com>
2025-04-16 16:28:21 -0500

    fixed banner in house

--- src/app/old-components/BannerStripe/BannerStripe.tsx
+++ src/app/old-components/BannerStripe/BannerStripe.tsx
@@ -8,10 +8,9 @@ export default function BannerStripe () {
     <div className='bg-[#8a5afa]'>
       <Link href='/'>
         <span className='sr-only'>Banner In House</span>
-        <video className='w-full max-w-2xl mx-auto h-auto max-h-[61.63px]' width={0} height={0} autoPlay loop muted playsInline>
-          <source src='/web/banner-in-house.webm' type='video/webm'/>
-          <source src='/web/banner-in-house.mp4' type='video/mp4'/>
-        </video>
+        <picture>
+          <img src='/web/banner-in-house.gif' className='w-full max-w-2xl mx-auto aspect-auto' alt='banner in house' />
+        </picture>
       </Link>
     </div>
   )
