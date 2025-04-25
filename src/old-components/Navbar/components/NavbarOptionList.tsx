f5cf2c82dd7c0c19bec5893b60208ebf57a3be51
david <david.cesar2126@gmail.com>
2025-04-25 09:51:22 -0500

    fixed

--- src/old-components/Navbar/components/NavbarOptionList.tsx
+++ src/old-components/Navbar/components/NavbarOptionList.tsx
@@ -8,16 +8,17 @@ export const NavbarOptionList = ({ options }: { options: Array<Options> }) => {
   return (
     <>
       <ul className='hidden justify-between xl:flex w-[1059px] max-w-[100%] items-center'>
-        <li>
-          {options.map((opt, pos) => (
-            <NavBarOption
-              key={pos}
-              {...opt}
-            />
-          ))}
+        
+        {options.map((opt, pos) => (
+          <NavBarOption
+            key={pos}
+            {...opt}
+          />
+        ))}
 
+        <li>
           <Link
-            className='relative text-white text-sm flex items-center capitalize font-bold bg-[#2A50E8] rounded-xl p-2 grid text-center w-full max-w-[150px]'
+            className='relative text-white text-sm grid items-center capitalize font-bold bg-[#2A50E8] rounded-xl p-2 text-center w-full max-w-[150px]'
             href='/buscar_certificado'
           >
             <span>Verificación de</span>
