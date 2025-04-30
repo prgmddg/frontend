4359cfc077636a7ccaac1ea6cabc4ffab386ca5e
david <david.cesar2126@gmail.com>
2025-04-30 13:26:45 -0500

    fix

--- src/app/(footer)/page.tsx
+++ src/app/(footer)/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
-import {MostrarCards, Stripe} from '@/old-components/Servicios'
-import {Carosuel, BeneficiosDeNuestrosProgramas, CentroDeCapacitacion, CertificiacionIso} from '..'
+import { MostrarCards, Stripe } from '@/old-components/Servicios'
+import { Carosuel, BeneficiosDeNuestrosProgramas, CentroDeCapacitacion, CertificiacionIso } from '..'
 import WhatsAppIcon from '@/app/old-components/WhatsAppIcon'
 import { useEffect, useState } from 'react'
 import getRequest from '@/helpers/getRequest'
@@ -10,13 +10,13 @@ import InitialPopups from '../old-components/InitialPopups/InitialPopups'
 export default function Home() {
   const [popups, setPopups] = useState<any>()
   const [errPopup, setErrPopup] = useState<any>()
- 
+
   useEffect(() => {
     getRequest('modal')
       .then((response) => {
         setPopups(response.res)
         setErrPopup(response.err)
- 
+
         if (window.scrollY > 925) window.scrollTo(0, 925)
       })
       .catch((error) => console.error(error))
@@ -27,13 +27,13 @@ export default function Home() {
       {popups?.length > 0 && !errPopup && (
         <InitialPopups initialPopups={popups} />
       )}
-      <WhatsAppIcon/>
-      <Carosuel/>
-      <Stripe/>
-      <MostrarCards program='proximos inicios' programSelector/>
-      <BeneficiosDeNuestrosProgramas/>
-      <CentroDeCapacitacion/>
-      <CertificiacionIso/>
+      <WhatsAppIcon />
+      <Carosuel />
+      <Stripe />
+      <MostrarCards program='proximos inicios' programSelector />
+      <BeneficiosDeNuestrosProgramas />
+      <CentroDeCapacitacion />
+      <CertificiacionIso />
     </>
   )
 }
--- src/old-components/ServiciosName/AcrodeonCursos.tsx
+++ src/old-components/ServiciosName/AcrodeonCursos.tsx
@@ -1,4 +1,5 @@
 'use client'
+
 import { programContext } from '@/context/ProgramContext'
 import useAcordeon from '@/hooks/useAcordeon'
 import programData from '@/types/programData'
