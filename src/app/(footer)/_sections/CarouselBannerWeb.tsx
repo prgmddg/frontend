c0cd17147557bc5f766744979e24fc90b9f98004
DavidDV21 <david.cesar2126@outlook.com>
2025-05-03 10:46:51 -0500

    fixed carousel

---
+++ src/app/(footer)/_sections/CarouselBannerWeb.tsx
@@ -0,0 +1,76 @@
+import Carousel from '@/components/Carousel'
+import Icon from '@/components/Icon'
+
+export default function CarouselBannerWeb () {
+  return (
+    <Carousel>
+      <div className='flex-shrink-0 w-full'>
+        <picture>
+          <source media='(max-width: 1024px)' srcSet='/web/banner-carousel-1-mob.webp' />
+          <img src='/web/banner-carousel-1.webp' className='w-full h-full max-h-[700px]' alt='Banner 1' loading='eager' />
+        </picture>
+      </div>
+      <div className='flex-shrink-0 w-full'>
+        <picture>
+          <source media='(max-width: 1024px)' srcSet='/web/banner-carousel-2-mob.webp' />
+          <img src='/web/banner-carousel-2.webp' className='w-full h-full max-h-[700px]' alt='Banner 2' loading='eager' />
+        </picture>
+      </div>
+      <div className='flex-shrink-0 w-full'>
+        <div
+          className='w-full aspect-[1009/745] lg:aspect-[1836/846] max-h-[700px] flex items-end justify-center'
+          style={{
+            backgroundImage: 'url(/web/bg-banner-carousel-3.webp)',
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+            backgroundPosition: 'center'
+          }}
+        >
+          { <div
+            className='flex header-container justify-between gap-[1rem]'
+          >
+            <section className='max-w-[546px] flex-col justify-center flex w-full mx-auto p-4'>
+              <div
+                className='mx-auto w-[100%] mob:mb-[27px] 560px:h-[69px] 560px:w-[100%] 1200px:w-fit bg-white rounded-[.5rem] h-[98px] flex items-center justify-between gap-[33px] 560px:gap-0 mb-[31px] px-[1rem] 560px:justify-between'
+              >
+                <picture>
+                  <img className='w-full max-w-[200px] aspect-auto' src='/web/logo.webp' width={0} height={0} alt='logo' loading='eager'  />
+                </picture>
+                <picture>
+                  <img className='w-full max-w-[200px] aspect-auto' src='/web/logo-iso.webp' width={0} height={0} alt='logo' loading='eager' />
+                </picture>
+              </div>
+              <h1 className='text-2xl lg:text-5xl font-bold text-white mb-[33px] w-fit'>
+                <span className='text-myYellow'>Capacitación</span>&nbsp; para
+                                Funcionarios y Servidores Públicos
+              </h1>
+              <div className='rounded-[.5rem] h-[60px] 600px:w-[100%] 1200px:w-fit w-[100%] 600px:px-[16px] flex justify-between gap-[11px] items-center bg-white px-[1rem] mx-auto'>
+                <Icon className='w-10 h-10' name='certificate' />
+                                
+                <p className='font-medium text-[18px] 600px:hidden'>
+                                  Certifícate y mejora tus oportunidades laborales
+                </p>
+                <span className='600px:block hidden leading-[18px] text-center'>
+                                  Certifícate y mejora tus oportunidades laborales
+                </span>
+                <span className='text-[45px] 600px:block hidden'>🤝</span>
+              </div>
+            </section>
+            <section className='flex items-end 1200px:hidden'>
+              <picture>
+                <img
+                  className='w-full aspect-auto'
+                  src='/web/group-people.webp'
+                  height={0}
+                  width={0}
+                  loading='eager'
+                  alt='grupo de ejecutivos sonriendo hacia al frente' />
+              </picture>
+            </section>
+          </div>
+          }
+        </div>
+      </div>
+    </Carousel>
+  )
+}
