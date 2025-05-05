dafffdecc1df986189d97bdcd086e5c16ed42374
DavidDV21 <david.cesar2126@outlook.com>
2025-05-05 12:18:11 -0500

    fixed change url link

--- src/app/old-components/BannerStripe/BannerStripe.tsx
+++ src/app/old-components/BannerStripe/BannerStripe.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function BannerStripe () {
   return (
     <div className='bg-[#8a5afa]'>
-      <Link href='/'>
+      <Link href='/inhouse'>
         <span className='sr-only'>Banner In House</span>
         <picture>
           <img src='/web/banner-in-house.webp' alt='' width={0} height={0} className='w-full max-w-2xl mx-auto aspect-auto' loading='lazy' />
