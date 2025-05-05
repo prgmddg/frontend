5295e589cee915dddba4fc8630229ec5d0ce65d5
DavidDV21 <david.cesar2126@outlook.com>
2025-05-05 12:14:30 -0500

    fixed metadata

--- src/helpers/getMetadata.ts
+++ src/helpers/getMetadata.ts
@@ -1,7 +1,7 @@
-import { OpenGraph } from 'next/dist/lib/metadata/types/opengraph-types'
-import getRequest from './getRequest'
 import apiurl from '@/types/apiUrl'
 import { Metadata } from 'next'
+import { OpenGraph } from 'next/dist/lib/metadata/types/opengraph-types'
+import getRequest from './getRequest'
 
 export default async function getMetadata (params: any, type: apiurl): Promise<Metadata> {
   const { name } = await params
@@ -14,7 +14,7 @@ export default async function getMetadata (params: any, type: apiurl): Promise<M
 
   return {
     ...seo,
-    keywords: type === 'seminarios' ? ['seminarios', 'cursos', 'diplomas'] : data.nuevo_seos.tags.length === 0 ? [] : data.nuevo_seos.tags,
+    keywords: type === 'seminarios' ? ['seminarios', 'cursos', 'diplomas'] : (data.nuevo_seos && data.nuevo_seos.tags.length > 0) ?  data.nuevo_seos.tags : [],
     openGraph: { ...seo, images: data.imagen || '', url: `https://desarrolloglobal.pe/${type}/${name}`, type: 'website' },
     authors: [{ name: 'Centro de Capacitación y Desarrollo Global', url: 'https://desarrolloglobal.pe' }],
     creator: 'Centro de Capacitación y Desarrollo Global',
