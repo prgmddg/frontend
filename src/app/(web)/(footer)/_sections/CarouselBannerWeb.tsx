5be712df4c2c9f38b4f94de091c9cfc022913f72
prgmddg <103072261+prgmddg@users.noreply.github.com>
2025-07-14 23:06:43 +0000

    fixed

--- src/app/(web)/(footer)/_sections/CarouselBannerWeb.tsx
+++ src/app/(web)/(footer)/_sections/CarouselBannerWeb.tsx
@@ -6,27 +6,27 @@ export default function CarouselBannerWeb () {
     <Carousel>
       <div className='flex-shrink-0 w-full'>
         <picture>
-          <source media='(max-width: 1024px)' srcSet='/web/banner-carousel-1-mob.webp' />
+          <source media='(max-width: 1024px)' srcSet='/web/b-carousel-1.webp' />
           <img src='/web/b-carousel-1.webp' className='w-full h-full max-h-[600px]' alt='Banner 1' loading='eager' />
         </picture>
       </div>
       <div className='flex-shrink-0 w-full'>
         <Link href='/cursos/contratacion-publicas-de-la-nueva-ley-general-32069'>
           <picture>
-            <source media='(max-width: 1024px)' srcSet='/web/banner-carousel-2-mob.webp' />
+            <source media='(max-width: 1024px)' srcSet='/web/b-carousel-2.webp' />
             <img src='/web/b-carousel-2.webp' className='w-full h-full max-h-[600px]' alt='Banner 2' loading='eager' />
           </picture>
         </Link>
       </div>
       <div className='flex-shrink-0 w-full'>
         <picture>
-          <source media='(max-width: 1024px)' srcSet='/web/banner-carousel-1-mob.webp' />
+          <source media='(max-width: 1024px)' srcSet='/web/b-carousel-3.webp' />
           <img src='/web/b-carousel-3.webp' className='w-full h-full max-h-[600px]' alt='Banner 2' loading='eager' />
         </picture>
       </div>
       <div className='flex-shrink-0 w-full'>
         <picture>
-          <source media='(max-width: 1024px)' srcSet='/web/banner-carousel-2-mob.webp' />
+          <source media='(max-width: 1024px)' srcSet='/web/b-carousel-4.webp' />
           <img src='/web/b-carousel-4.webp' className='w-full h-full max-h-[600px]' alt='Banner 2' loading='eager' />
         </picture>
       </div>
