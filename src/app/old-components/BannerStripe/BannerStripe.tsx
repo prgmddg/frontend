534d8bbd499d8bc831a88aec74d9d296546cdee1
prgmddg <103072261+prgmddg@users.noreply.github.com>
2025-07-15 20:11:23 +0000

    fixed

--- src/app/old-components/BannerStripe/BannerStripe.tsx
+++ src/app/old-components/BannerStripe/BannerStripe.tsx
@@ -6,7 +6,7 @@ export default function BannerStripe () {
       <Link href='/inhouse'>
         <span className='sr-only'>Banner In House</span>
         <picture>
-          <img src='/web/banner-in-house.webp' alt='' width={0} height={0} className='w-full max-w-[1200px] mx-auto aspect-auto' loading='lazy' />
+          <img src='/web/banner-in-house.webp' alt='' width={0} height={0} className='w-full max-w-[1400px] mx-auto aspect-auto' loading='lazy' />
         </picture>
       </Link>
     </div>
