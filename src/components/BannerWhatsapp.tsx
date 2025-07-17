01e8801828a58afe27e0f9daf94faf062cb22cff
DavidDV21 <david.cesar2126@outlook.com>
2025-07-17 18:30:23 -0500

    fixed banner

--- src/components/BannerWhatsapp.tsx
+++ src/components/BannerWhatsapp.tsx
@@ -2,8 +2,8 @@ export default function BannerWhatsapp () {
   return (
     <a href='https://whatsapp.com/channel/0029VbAv0QeGOj9rVz73Ci2g' target='_blank' className='w-full'>
       <div className='bg-green-500'>
-        <div className='flex flex-col flex-wrap items-center justify-between max-w-5xl gap-0 p-4 mx-auto text-white md:gap-4 font-inter sm:flex-row'>
-          <div className='max-w-full text-lg leading-relaxed text-center sm:text-xl md:text-2xl sm:text-left sm:max-w-3/4'>
+        <div className='flex flex-col flex-wrap items-center justify-between max-w-5xl gap-0 p-4 mx-auto text-white md:gap-4 font-inter lg:flex-row'>
+          <div className='max-w-full text-lg leading-relaxed text-center sm:text-xl md:text-2xl lg:text-left sm:max-w-3/4'>
             <p className='mb-1 font-bold'>
               <span className=''>👋</span> ¡Únete a nuestro canal de WhatsApp!
             </p>
@@ -12,7 +12,7 @@ export default function BannerWhatsapp () {
             ¡Es fácil y rápido!
             </p>
           </div>
-          <div className='flex items-center justify-center px-6 py-3 mt-4 text-base font-bold text-green-600 transition duration-300 ease-in-out transform bg-white rounded-full shadow-md hover:bg-gray-100 sm:text-lg hover:scale-105 sm:mt-0' rel='noopener'>
+          <div className='hidden md:flex items-center justify-center px-6 py-3 mt-4 text-base font-bold text-green-600 transition duration-300 ease-in-out transform bg-white rounded-full shadow-md hover:bg-gray-100 sm:text-lg hover:scale-105 sm:mt-0' rel='noopener'>
             <picture>
               <img decoding='async' src='https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg' alt='WhatsApp' className='w-full mr-2 max-w-6 aspect-auto' width={0} height={0} title='Blog de Gestión Pública' />
             </picture>
